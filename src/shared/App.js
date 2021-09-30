ac09546041f71bc112544d7b63c028a620ec500e
kimbumsoo0820 <kbs2082@naver.com>
2021-09-30 17:37:03 +0900

    login

--- src/shared/App.js
+++ src/shared/App.js
@@ -4,12 +4,14 @@ import "./App.css";
 import { BrowserRouter, Route } from "react-router-dom";
 import React from "react";
 import PostList from "../pages/PostList";
+import Login from "../pages/Login";
 
 function App() {
   return (
     <React.Fragment>
       <BrowserRouter>
         <Route path="/" exact component={PostList}></Route>
+        <Route path="/login" exact component={Login}></Route>
       </BrowserRouter>
     </React.Fragment>
   );
